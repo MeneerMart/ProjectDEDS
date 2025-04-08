84d6387f3fb35fa361f3e6f3f6c5d90aaf3ffd27
MeneerMart <102212872+MeneerMart@users.noreply.github.com>
2025-04-08 22:06:37 +0200

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,18 @@ const App = () => {
     }, []);
 
     return (
-            <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
-                <div className="shadow-xl rounded-2xl overflow-hidden bg-white">
-                    <iframe
-                        title="Project DEDS"
-                        width={dimensions.width - 20}
-                        height={dimensions.height - 20}
-                        src="https://app.powerbi.com/view?r=eyJrIjoiMjQyMTk1NzktZjY2Zi00ZDU0LWIwNzItNjA3ZWFkNWVhZWFkIiwidCI6ImEyNTg2YjliLWY4NjctNGIzYy05MzYzLTViNDM1YzVkYmM0NSIsImMiOjh9"
-                        allowFullScreen
-                        className="border-0"
-                    ></iframe>
-                </div>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
+            <div className="shadow-xl rounded-2xl overflow-hidden bg-white">
+                <iframe
+                    title="Project DEDS v2"
+                    width={dimensions.width - 20}
+                    height={dimensions.height - 20}
+                    src="https://app.powerbi.com/view?r=eyJrIjoiZTQ1ZDk0YmUtMGM2MS00ZTJjLWE2YWQtZDVmMTZkYWFmYTg3IiwidCI6ImEyNTg2YjliLWY4NjctNGIzYy05MzYzLTViNDM1YzVkYmM0NSIsImMiOjh9"
+                    frameborder="0"
+                    allowFullScreen="true">
+                </iframe>
             </div>
+        </div>
     );
 };
 
